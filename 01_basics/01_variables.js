26be5e6555ecb9f9310c974d8fdcc6d9e3f6d83d
VikashDubey32 <133970574+VikashDubey32@users.noreply.github.com>
2023-07-27 15:22:32 +0000

    datatypes

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -5,7 +5,8 @@ var emailId= "vikash@google.com"
 
 // accountId=2 // not allowed
 
-// Prefer Var is not used
+// Prefer Var is not used 
+//because of issue in block scope and functional scope
 
 console.log(accountId);
 console.table([accountId,cityName, emailId]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,16 @@
+"use strict ";//treat JS code as newer version
+
+//alert(3 + 3) // we are use Nodejs, not browser
+
+let name = "vikash"
+let age = 25
+
+
+// null => standalone value
+// symbol => unique
+console.log(name);
+console.log(age);
+console.log(typeof "vikash");
+
+console.log(typeof null);// object
+console.log(typeof undefined); // undefined
