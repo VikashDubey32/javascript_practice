56078915dd4c8229ae0c571365e9845caf1cbaf2
VikashDubey32 <133970574+VikashDubey32@users.noreply.github.com>
2023-07-27 14:48:12 +0000

    config done and variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId=11233
+let cityName = "Mumbai"
+var emailId= "vikash@google.com"
+
+
+// accountId=2 // not allowed
+
+// Prefer Var is not used
+
+console.log(accountId);
+console.table([accountId,cityName, emailId]);
