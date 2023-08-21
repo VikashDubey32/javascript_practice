037ae84362476bea45b1b28d9f70e9cdea676ac3
VikashDubey32 <133970574+VikashDubey32@users.noreply.github.com>
2023-08-21 14:09:30 +0000

    datatypes

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,29 @@
+
+let marks=false
+
+let convert = Number(marks)
+
+
+//console.log(typeof convert)
+//console.log(convert)
+
+// "22ABC" => NAN 
+// true => 1 and false => 0
+
+
+let bool = -1
+
+let databool = Boolean(bool)
+
+// console.log(typeof databool)
+// console.log(databool)
+
+// "vikash" => true
+// "" => false
+
+
+let gamecounter = 100
+let incr = gamecounter++
+
+console.log(gamecounter);
+console.log(incr);
